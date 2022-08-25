1f24877894257c0e43b9d1fc697e65697a158ffa
antoniodebrito <antonio11brito@gmail.com>
2022-08-25 18:38:49 -0300

    add new files

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const minutes = document.querySelector(".minutes");
+const seconds = document.querySelector(".seconds");
+const play = document.querySelector(".play");
+const stop = document.querySelector(".stop");
+const addMinutes = document.querySelector(".add-minutes");
+const subtractMinutes = document.querySelector(".subtract-minutes");
+
+
+play.addEventListener('click', () => {
+  
+})
